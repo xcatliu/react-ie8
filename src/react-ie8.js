25fdec69c0fcd326d88b86bc57cabdcd37d29459
xcatliu <xcatliu@gmail.com>
2015-12-26 00:20:52 +0800

    Update src

--- src/react-ie8.js
+++ src/react-ie8.js
@@ -15,7 +15,7 @@ const defaultOptions = {
 export default function reactIE8(options = {}) {
   const finalOptions = Object.assign({}, defaultOptions, options);
   if (finalOptions['es5-shim']) require('es5-shim');
-  if (finalOptions['es5-sham']) require('es5-sham');
+  if (finalOptions['es5-sham']) require('es5-shim/es5-sham');
   if (finalOptions['console-polyfill']) require('console-polyfill');
   if (finalOptions['html5shiv']) require('html5shiv');
   // Object.keys(finalOptions).filter((key) => finalOptions[key]).
