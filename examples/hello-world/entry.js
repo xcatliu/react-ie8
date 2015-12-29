210947bc5997d206a6fa652ab0afaf2d5213f9d4
xcatliu <xcatliu@gmail.com>
2015-12-29 23:36:42 +0800

    examples/hello-world works

--- examples/hello-world/entry.js
+++ examples/hello-world/entry.js
@@ -1,8 +1,22 @@
-import reactIE8 from 'react-ie8';
-reactIE8();
+/**
+ * CANNOT use `import` to import `react-ie8`,
+ * because `import` will be transformed to `Object.defineProperty`,
+ * which doesn't exists in IE8 (but will be polyfilled after `require('react-ie8')()` executed).
+ */
+// import reactIE8 from 'react-ie8';
+// reactIE8();
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+require('react-ie8')();
+
+/**
+ * CANNOT use `import` to import `react` or `react-dom`,
+ * because `import` will run `react` before `require('react-ie8')()`.
+ */
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 ReactDOM.render(
   <h1>Hello World</h1>,
--- src/react-ie8.js
+++ src/react-ie8.js
@@ -12,11 +12,9 @@ const defaultOptions = {
   // 'font-face': false,
 };
 
-export default function reactIE8(options = {}) {
+module.exports = function reactIE8(options = {}) {
   const finalOptions = Object.assign({}, defaultOptions, options);
   if (finalOptions['es5-shim']) require('es5-shim');
   if (finalOptions['es5-sham']) require('es5-shim/es5-sham');
   if (finalOptions['console-polyfill']) require('console-polyfill');
-  // if (finalOptions['html5shiv']) require('html5shiv');
-  // Object.keys(finalOptions).filter((key) => finalOptions[key]).
-}
+};
